f71d4ae9adfdef6f93dd3044f8064daee31d00f7
Darshpatel3016 <darshpatel3016@gmail.com>
2025-02-14 19:09:46 +0530

    cc

--- backend/Routes/DisplayData.js
+++ backend/Routes/DisplayData.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/foodData", (req, res) => {
+router.get("/foodData", (req, res) => {
 
     try {
-        console.log(global.food_item, global.foodCategory)
-        res.send([global.food_item, global.foodCategory])
+        console.log(global.food_item, global.foodCategory);
+
+        if(!global.food_item || global.foodCategory) {
+            return res.status(500).json ({error: "Food data not available"});
+        }
+        res.json([global.food_item, global.foodCategory])
+
     } catch (error) {
-        console.log(error.message);
-        res.send("server error");
+        console.log("Error fetching food data:",error.message);
+        res.status(500).json({error: "Internal server error"});
     }
 });
 
