c02794fc5dcec2c07ff556350970df3e41392ede
Darshpatel3016 <darshpatel3016@gmail.com>
2025-02-15 13:04:31 +0530

    cors set

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express()
 const port = 5000
 const mongoDB = require("./db");
-const cors = require("cors");
 
 mongoDB();
 
 app.use(cors({
     origin: "https://gofood-frontendd.onrender.com",
-    methods: "GET, POST",
-    allowedHeaders: "Content-Type",
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Content-Type, Authorization",
     credentials: true
 }));
 
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://gofood-frontendd.onrender.com");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Method",
-        "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    next();
-})
 
 app.use(express.json())
 app.use("/api", require("./Routes/CreateUser"));
